943639d784c953424bf8443bb8cc3112c95eb761
Sarah Hubschman <sarahhubschmail@gmail.com>
2018-04-10 18:10:00 -0400

    init commit

---
+++ src/app/config.js
@@ -0,0 +1,14 @@
+let secrets = require('../../secrets');
+
+module.exports = {
+
+  // Change this to your firebase configuration! (Add Firebase to your web app)
+    FIREBASE_CONFIG: {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: "namecodes-c5201.firebaseapp.com",
+      databaseURL: "https://namecodes-c5201.firebaseio.com",
+      projectId: "namecodes-c5201",
+      storageBucket: "",
+      messagingSenderId: "166687077294"
+    },
+};
